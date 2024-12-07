fa6c183d2763e213c54661309b37635693b481cd
bhomik005 <bhomik005@gmail.com>
2024-12-07 16:15:42 +1100

    added token data structure and potential token types as string

---
+++ token/token.go
@@ -0,0 +1,36 @@
+package token
+
+type TokenType string
+
+// token is going to store the token type and literal string
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// declaring the possible token types as constants
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 134583
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
